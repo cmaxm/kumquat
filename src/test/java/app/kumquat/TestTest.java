b8b28c6f6670a6539082ab72aaebcf53c0236a52
Peter Filina <peter.filina@hotmail.com>
2019-01-16 21:17:25 +0100

    First commit

---
+++ src/test/java/app/kumquat/TestTest.java
@@ -0,0 +1,14 @@
+package app.kumquat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
